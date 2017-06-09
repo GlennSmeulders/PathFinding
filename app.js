787a94d2c29897929b694df67f7c9e4e6574073e
Mich Van Mooter <mich@vanmooter.be>
2017-06-09 16:11:42 +0200

    Level 2 + slight refactor

--- app.js
+++ app.js
@@ -1,27 +1,42 @@
+var axios = require('axios');
 var PF = require('pathfinding');
 
-console.log("yolo");
-var string = "######F###\r\n##.....###\r\n##.#######\r\n#........#\r\n#.######.#\r\n#........#\r\n#######.##\r\n###.....##\r\n###S######";
-var spacesplit = string.split("\r\n");
-var hashsplit = string.split("#");
-var test = [,];
-var tests = new Array();
-console.log(spacesplit);
-console.log(spacesplit[0]);
+axios
+  .get('http://mazeretreat.azurewebsites.net/mazes/51b8269c-00e2-4486-ac05-f4490942c0c9')
+  .then((response) => console.log(response.data))
+  .catch(console.error);
 
+// Now manually convert the maze string to a matrix like this :D
 
 var matrix = [
-    [1,1,1,1,1,1,0,1,1,1],
-    [1,1,0,0,0,0,0,1,1,1],
-    [1,1,0,1,1,1,1,1,1,1],
-    [1,0,0,0,0,0,0,0,0,1],
-    [1,0,1,1,1,1,1,1,0,1],
-    [1,0,0,0,0,0,0,0,0,1],
-    [1,1,1,1,1,1,1,0,1,1],
-    [1,1,1,0,0,0,0,0,1,1],
-    [1,1,1,0,1,1,1,1,1,1],
+  [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+  [1, 1, 0, 0, 0, 1, 1, 0, 0, 0, 1, 1],
+  [1, 1, 0, 1, 0, 1, 1, 0, 1, 0, 1, 1],
+  [1, 1, 0, 1, 0, 1, 1, 0, 1, 0, 1, 1],
+  [0, 0, 0, 1, 0, 1, 1, 0, 1, 0, 0, 0],
+  [1, 1, 0, 1, 0, 1, 1, 0, 1, 0, 1, 1],
+  [1, 0, 0, 1, 0, 1, 1, 0, 1, 0, 0, 1],
+  [1, 1, 1, 1, 0, 0, 0, 0, 1, 1, 1, 1],
+  [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
 ];
+
 var grid = new PF.Grid(matrix);
 var finder = new PF.AStarFinder();
-var path = finder.findPath(3,8,6,0,grid);
-console.log(path);
+var pathToFirstPortal = finder.findPath(0, 4, 1, 6, grid);
+var pathFromSecondPortalToEnd = finder.findPath(10, 6, 11, 4, grid);
+
+var solutionString = pathToFirstPortal
+  .concat(pathFromSecondPortalToEnd)
+  .map(coordinate => `${coordinate[0]},${coordinate[1]}`)
+  .reduce((prev, curr) => prev + curr + ';', '');
+
+const solution = {
+  TeamName: 'Rockerteers',
+  MazeId: '51b8269c-00e2-4486-ac05-f4490942c0c9',
+  Solution: solutionString,
+};
+
+axios
+  .post('http://mazeretreat.azurewebsites.net/solutions/51b8269c-00e2-4486-ac05-f4490942c0c9', solution)
+  .then(console.log)
+  .catch(console.error);
